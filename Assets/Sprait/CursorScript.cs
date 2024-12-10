dd5298ea7c795d2b277788fd68527e170ac9e06b
Kirijena <kirijena28@gmail.com>
2024-12-10 15:29:21 +0200

    cursors

---
+++ Assets/Sprait/CursorScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+    public Texture2D[] cursors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DefaultCursor();
+    }
+
+    // Update is called once per frame
+    void DefaultCursor()
+    {
+        Cursor.SetCursor(cursors[0], Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
